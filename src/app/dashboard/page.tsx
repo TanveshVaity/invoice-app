af31d2301ca3d6aa3f549b8e89dbe5dd467cfe5a
TanveshVaity <tanveshvaity99@gmail.com>
2025-02-07 03:12:08 +0530

    added home and configuration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
 
 export default function Home() {
   return (
       <div className="flex flex-col items-center justify-center min-h-screen text-center">
          <main className="flex flex-col gap-6 max-w-5xl mx-auto">
             <h1 className="text-5xl font-bold">
-               Invoice App
+               Dashboard
             </h1>
-            <div className="mt-4">
-               <Button asChild>
-                  <Link href="/dashboard">Sign In</Link>
-               </Button>
-            </div>
          </main>
       </div>
   );
