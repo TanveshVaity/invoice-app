38b80ea6b4049d143ceb6f4963dad8eba8f275e7
TanveshVaity <tanveshvaity99@gmail.com>
2025-02-15 05:58:01 +0530

    listing invoices

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,8 +11,11 @@ import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button";
 import { CirclePlus } from 'lucide-react';
 import Link from "next/link";
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const invoices = await db.select().from(Invoices);
     return (
         <main className="flex flex-col items-center justify-center text-center gap-6 max-w-5xl mx-auto my-12">
             <div className="flex justify-between  w-full">
@@ -49,23 +52,56 @@ export default function Dashboard() {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell className="text-left p-4">
-                            <span className="font-semibold">10/31/2024</span>
+                    {invoices.map(invoice =>{
+                        return (
+                            <TableRow key={invoice.id}>
+                                <TableCell className="text-left p-0">
+                                    <Link href={`invoices/${invoice.id}`} className="p-4 block font-semibold">
+                                        {new Date(invoice.createTs).toLocaleDateString()}
+                                    </Link>
+                                </TableCell>
+                                <TableCell className="text-left p-0">
+                                    <Link href={`invoices/${invoice.id}`} className="p-4 block font-semibold">
+                                        tan
+                                    </Link>
+                                </TableCell>
+                                <TableCell className="text-left p-0">
+                                    <Link href={`invoices/${invoice.id}`} className="p-4 block">
+                                        tan@planetexpress.com
+                                    </Link>
+                                </TableCell>
+                                <TableCell className="text-center p-0">
+                                    <Link href={`invoices/${invoice.id}`} className="p-4 block">
+                                        <Badge className="rounded-full bg-slate-600">
+                                            {invoice.status}
+                                        </Badge>
+                                    </Link>
+                                </TableCell>
+                                <TableCell className="text-right p-0">
+                                    <Link href={`invoices/${invoice.id}`} className="p-4 block">
+                                        Rs.{invoice.value / 100}
+                                    </Link>
+                                </TableCell>
+                            </TableRow>
+                        )
+                    })}
+                    {/*  <TableRow>
+                         <TableCell className="text-left p-4">
+                             <span className="font-semibold">10/31/2024</span>
                         </TableCell>
-                        <TableCell className="text-left p-4">
+                         <TableCell className="text-left p-4">
                             <span className="font-semibold">tan</span>
-                        </TableCell>
-                        <TableCell className="text-left p-4">
-                            <span>tan@planetexpress.com</span>
-                        </TableCell>
-                        <TableCell className="text-center p-4">
-                            <Badge className="rounded-full bg-slate-600">Open</Badge>
-                        </TableCell>
-                        <TableCell className="text-right p-4">
-                            <span>$250.00</span>
-                        </TableCell>
-                    </TableRow>
+                         </TableCell>
+                         <TableCell className="text-left p-4">
+                             <span>tan@planetexpress.com</span>
+                         </TableCell>
+                         <TableCell className="text-center p-4">
+                             <Badge className="rounded-full bg-slate-600">Open</Badge>
+                         </TableCell>
+                         <TableCell className="text-right p-4">
+                             <span>$250.00</span>
+                         </TableCell>
+                     </TableRow> */}
                 </TableBody>
             </Table>
         </main>
