c0445e0b4061a19629786f43a37f0fee0a31f4e6
TanveshVaity <tanveshvaity99@gmail.com>
2025-02-21 03:30:39 +0530

    multi factor authentication

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,7 @@
 import * as Clerk from '@clerk/elements/common';
 import * as SignIn from '@clerk/elements/sign-in';
 import { Button } from '@/components/ui/button';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function SignInPage() {
   return (
@@ -89,6 +90,89 @@ export default function SignInPage() {
             <Clerk.Link
               navigate="sign-up"
               className="font-medium text-zinc-950 decoration-zinc-950/20 underline-offset-4 outline-none hover:text-zinc-700 hover:underline focus-visible:underline"
+            >
+              Sign Up
+            </Clerk.Link>
+          </p>
+        </SignIn.Step>
+        <SignIn.Step
+          name="verifications"
+          className="relative isolate w-full space-y-8 rounded-2xl px-4 py-10  sm:w-96 sm:px-8"
+        >
+          <header className="text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="lucide lucide-briefcase mx-auto size-10 text-zinc-950"
+              viewBox="0 0 24 24"
+            >
+              <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+              <rect width="20" height="14" x="2" y="6" rx="2"></rect>
+            </svg>
+            <h1 className="mt-4 text-xl font-medium tracking-tight text-zinc-950">
+              Verify Authenticator Code
+            </h1>
+          </header>
+          <Clerk.GlobalError className="block text-sm text-rose-400" />
+          <SignIn.Strategy name="totp">
+            <Clerk.Field name="code" className="group/field relative">
+              <Clerk.Label className="sr-only">
+                Phone code
+              </Clerk.Label>
+              <Clerk.Input
+                type="otp"
+                required
+                className="flex justify-center gap-1"
+                render={({ value, status }) => (
+                  <div
+                    data-status={status}
+                    className="relative h-9 w-8 mr-3 rounded-md bg-white ring-1 ring-inset ring-zinc-950 data-[status=selected]:bg-sky-400/10 data-[status=selected]:shadow-[0_0_8px_2px_theme(colors.sky.400/30%)] data-[status=selected]:ring-sky-400"
+                  >
+                    <AnimatePresence>
+                      {value && (
+                        <motion.span
+                          initial={{ opacity: 0, scale: 0.75 }}
+                          animate={{ opacity: 1, scale: 1 }}
+                          exit={{ opacity: 0, scale: 0.75 }}
+                          className="absolute inset-0 flex items-center justify-center text-zinc-950"
+                        >
+                          {value}
+                        </motion.span>
+                      )}
+                      {value}
+                    </AnimatePresence>
+                    {status === 'cursor' && (
+                      <motion.div
+                        layoutId="otp-input-focus"
+                        transition={{ ease: [0.2, 0.4, 0, 1], duration: 0.2 }}
+                        className="absolute inset-0 z-10 rounded-[inherit] border border-sky-400 bg-sky-400/10 shadow-[0_0_8px_2px_theme(colors.sky.400/30%)]"
+                      />
+                    )}
+                  </div>
+                )}
+              />
+              <Clerk.FieldError className="mt-2 block text-xs text-rose-400" />
+            </Clerk.Field>
+            <SignIn.Action
+              submit
+              asChild
+            >
+              <Button className='w-full font-bold'>
+                Continue
+              </Button>
+            </SignIn.Action>
+          </SignIn.Strategy>
+          <p className="text-center text-sm text-zinc-950">
+            No account?{' '}
+            <Clerk.Link
+              navigate="sign-up"
+              className="text-zinc-950 decoration-white/30 underline-offset-4 outline-none hover:underline focus-visible:underline"
             >
               Sign Up
             </Clerk.Link>
